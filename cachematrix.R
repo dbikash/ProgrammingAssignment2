f87da048d07464c4f46df06a402a4eafe4e4e64a
dbikash <dbikash@gmail.com>
2015-02-19 11:19:09 +0530

    Commit of Programming Assignment 2
    Added comments to improve readability

--- cachematrix.R
+++ cachematrix.R
@@ -14,12 +14,20 @@
 ## 4. Get the value of its inverse
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
+    
+    ## 1. Set function: store matrix in parent environment
     set <- function(y) {
       x <<- y
       inv <<- NULL
     }
+    
+    ## 2. Get function: retrieve matrix from parent environment
     get <- function() x
+    
+    ## 3. Set inverse in parent environment
     setinverse <- function(inverse) inv <<- inverse
+    
+    ## 4. Get inverse from parent environment
     getinverse <- function() inv
     list(set = set, get = get,
          setinverse = setinverse,
@@ -30,15 +38,20 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Returns the inverse of the 'special matrix' object created with the help of the
 ## above function. If present in cache, it returns inverse from cache. Else it
 ## calculates the inverse and stores in cache. Matrix is assumed to be invertible, so
-## error checking is done
+## no error checking is done
 cacheSolve <- function(x, ...) {
+  
+  ## Return cached inverse if available
   inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
+  
+  ## Else, calculate inverse and cache
   data <- x$get()
   inv <- solve(data)
   x$setinverse(inv)
+  
   inv
 }
